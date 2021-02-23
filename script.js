9a93c92c687455c2693eb9995dd69dcaaa04762c
Vikas Singh <44vikasrajput44@gmail.com>
2021-02-23 16:34:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+var algopro=['Shortest Path Finder using Visualization',
+'a new programming language',
+'Sorting using Visualization',
+'Code converter from one language to other',
+'Auto Word predictor',
+'Recommendation engines using trie data structure',
+'Make your own operating system like linux/windows','Make your own server to store data'
+ ]
+
+function algoproject(){
+var randomnumber= Math.floor(Math.random()*(algopro.length));
+document.getElementById('algo_display').innerHTML= algopro[randomnumber];
+}
+
+
+
+var jobpro=['Shortest Path Finder( to use in google maps)',
+'a new programming language',
+'A new library',
+'Code converter from one language to other',
+'Auto Code bug detector and fixer',
+'Real time video to video language translator(like amazon polly)',
+'Real time audio to audio language translator','Your own operating system like linux/windows'
+ ]
+
+function jobproject(){
+var randomnumber= Math.floor(Math.random()*(jobpro.length));
+document.getElementById('job_display').innerHTML= jobpro[randomnumber];
+}
+
+
+
+var projects=['Random Quote Generator',
+'To Do List App',
+'Habit Tracker',
+'Productivity Tracker',
+'Amazon Clone',
+'Zoom Clone',
+'Clubhouse Clone','Spam Email Classifier','Twitter Sentiment Analysis','Music Recommendation System','Fake News Detector','Stock Price Predictor'
+ ]
+
+function newproject(){
+var randomnumber= Math.floor(Math.random()*(projects.length));
+document.getElementById('project_display').innerHTML= projects[randomnumber];
+}
+
+ var wfh=['Virtual attendance app',
+'Conference app','Virtual Live Class app',
+'Zoom clone', 
+'Microsoft team Clone',
+'Clubhouse Clone','Remote desktop control app','Music Recommendation System','Screen sharing app','WFH tips app'
+ ]
+
+function Work_From_Home(){
+var randomnumber= Math.floor(Math.random()*(wfh.length));
+document.getElementById('wfh_display').innerHTML= wfh[randomnumber];
+}
+
+ var farmp=['Rainfall prediction',
+'Crop Moisture/water/insectide Requirement Teller',
+'Best possible crop Suggestion',
+'Farming Awareness app',
+'Farming Automation using IoT','Loan app for Farmers'
+ ]
+
+function farmingpro(){
+var randomnumber= Math.floor(Math.random()*(farmp.length));
+document.getElementById('wfh_display').innerHTML= farmp[randomnumber];
+}
+
+ 
+ var fitp=['Virtual Yoga instructor app',
+'Water reminder alarm', 'diet planner', 
+'Calorie Tracker',
+'Foot Step Counter','Diet and Habit Tracker'
+ ]
+
+function fitpro(){
+var randomnumber= Math.floor(Math.random()*(fitp.length));
+document.getElementById('fit_display').innerHTML= fitp[randomnumber];
+}
+
+   var pollp=['Air Quality app',
+'Pollution reducing Tips app',
+'Environment News app',
+'Grow 3 plant a month app','Herbs benefit teller app', 'Home Farming Guide app', 'Detailed Manure Guide', 'Detailed pesticide Guide'
+ ]
+
+function pollpro(){
+var randomnumber= Math.floor(Math.random()*(pollp.length));
+document.getElementById('poll_display').innerHTML= pollp[randomnumber];
+}
+
+var edu=['Auto Grammar checker',
+'Duolingo clone',
+'Auto grammar corrector',
+' Teaching with simulation/visualization','online doubt solver app', 'click pic, get answer', 'flashcard app', 'daily current affairs', 'dictionary app', 'AR Based learning', 'VR based learning' , 'News Reader Assistant'
+ ]
+
+function edupro(){
+var randomnumber= Math.floor(Math.random()*(edu.length));
+document.getElementById('edu_display').innerHTML= edu[randomnumber];
+}
